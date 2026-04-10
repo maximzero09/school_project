60ab645d2d96161910de7e1b4bf54c793c83960f
maximzero09 <maximzero09@outlook.com>
2026-04-10 15:31:05 +0300

    Added lists

---
+++ school_project/store.cs
@@ -0,0 +1,11 @@
+public class Store
+{
+  public static  List<Product> products = new List<Product>();
+    List<string> orders = new List<string>();
+    List<string> clients = new List<string>();
+    
+
+    
+    
+
+}
